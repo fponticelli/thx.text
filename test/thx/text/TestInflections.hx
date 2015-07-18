ff8a19704a685f6ef9f1f72203bf038ac404faad
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-07-17 20:33:25 -0600

    added test

--- test/thx/text/TestInflections.hx
+++ test/thx/text/TestInflections.hx
@@ -1,9 +1,27 @@
 package thx.text;
 
-import thx.text.Inflections;
+import utest.Assert;
+using thx.text.Inflections;
 
 class TestInflections {
-  public function new() {
-    
+  public function new() { }
+
+  public function testUncountable() {
+    Assert.equals("information", "information".pluralize());
+    Assert.equals("news", "news".pluralize());
+  }
+
+  public function testPluralize() {
+    Assert.equals("days", "day".pluralize());
+    Assert.equals("women", "woman".pluralize());
+    Assert.equals("autobuses", "autobus".pluralize());
+    Assert.equals("quizzes", "quiz".pluralize());
+  }
+
+  public function testSingularize() {
+    Assert.equals("day", "days".singularize());
+    Assert.equals("woman", "women".singularize());
+    Assert.equals("autobus", "autobuses".singularize());
+    Assert.equals("quiz", "quizzes".singularize());
   }
 }
