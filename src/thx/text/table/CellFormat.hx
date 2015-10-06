e4dc79341f2d4f7c835630741dc26cb10af31be4
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-10-05 20:53:25 -0600

    notes

--- src/thx/text/table/CellFormat.hx
+++ src/thx/text/table/CellFormat.hx
@@ -8,3 +8,26 @@ class CellFormat<T> {
     this.postFormat = null == postFormat ? function(s) return s : postFormat;
   }
 }
+
+/*
+var table = new Table();
+table.headerColumns(1);
+table.headerRows(1);
+table.addIndex();
+table.setCellFormat(col, row, Table.float);
+table.setColumnFormat(col, Table.bool);
+
+table.header([...]);
+table.row([...]);
+table.format([
+  ["name",  "age", "income",  "is person"],
+  ["franco",  43,   100.123,   true],
+  ["thx",      6,   999.99,    false],
+  ["haxe",  null,  1000,       false]
+]);
+
+// TODO
+//  * support vertical align
+//  * fixed width
+
+*/
