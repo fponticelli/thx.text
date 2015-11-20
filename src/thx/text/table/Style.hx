92bfd5ed393e42778238735e01890875989ec703
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-19 21:16:42 -0700

    added minwidth/minheight/aligner

--- src/thx/text/table/Style.hx
+++ src/thx/text/table/Style.hx
@@ -11,8 +11,11 @@ import thx.text.table.CellValue;
 class Style implements IStyle {
   @:isVar public var type(get, set) : CellType;
   @:isVar public var maxHeight(get, set) : Null<Int>;
+  @:isVar public var minHeight(get, set) : Int;
   @:isVar public var maxWidth(get, set) : Null<Int>;
+  @:isVar public var minWidth(get, set) : Int;
   @:isVar public var formatter(get, set) : Formatter;
+  @:isVar public var aligner(get, set) : Aligner;
   public function new() {}
 
   function get_type()
@@ -27,17 +30,35 @@ class Style implements IStyle {
   function set_maxHeight(value : Null<Int>) : Null<Int>
     return maxHeight = value;
 
+  function get_minHeight() : Int
+    return minHeight;
+
+  function set_minHeight(value : Int) : Int
+    return minHeight = value;
+
   function get_maxWidth() : Null<Int>
     return maxWidth;
 
   function set_maxWidth(value : Null<Int>) : Null<Int>
     return maxWidth = value;
 
+  function get_minWidth() : Int
+    return minWidth;
+
+  function set_minWidth(value : Int) : Int
+    return minWidth = value;
+
   function get_formatter() : Formatter
     return formatter;
 
   function set_formatter(value : Formatter) : Formatter
     return formatter = value;
+
+  function get_aligner() : Aligner
+    return aligner;
+
+  function set_aligner(value : Aligner) : Aligner
+    return aligner = value;
 }
 
 class CompositeStyle extends Style {
@@ -59,17 +80,35 @@ class CompositeStyle extends Style {
   override function set_maxHeight(value : Null<Int>) : Null<Int>
     return maxHeight = value;
 
+  override function get_minHeight() : Int
+    return getProperty("minHeight");
+
+  override function set_minHeight(value : Int) : Int
+    return minHeight = value;
+
   override function get_maxWidth() : Null<Int>
     return getProperty("maxWidth");
 
   override function set_maxWidth(value : Null<Int>) : Null<Int>
     return maxWidth = value;
 
+  override function get_minWidth() : Int
+    return getProperty("minWidth");
+
+  override function set_minWidth(value : Int) : Int
+    return minWidth = value;
+
   override function get_formatter() : Formatter
     return getProperty("formatter");
 
   override function set_formatter(value : Formatter) : Formatter
     return formatter = value;
+
+  override function get_aligner() : Aligner
+    return getProperty("aligner");
+
+  override function set_aligner(value : Aligner) : Aligner
+    return aligner = value;
 
   // TODO, this should expand to a macro
   function getProperty<T>(name : String) : Null<T> {
@@ -89,10 +128,12 @@ class DefaultStyle implements IStyle {
   public static var instance(default, null) : DefaultStyle = new DefaultStyle();
   public static var defaultType : CellType = BodyCompact;
   public static var defaultMaxHeight : Null<Int> = null;
+  public static var defaultMinHeight : Int = 0;
   public static var defaultMaxWidth : Null<Int> = null;
+  public static var defaultMinWidth : Int = 0;
   public static var defaultCulture : Culture = Embed.culture("en-us");
   public static var defaultIntFormatter : Int -> String = function(v : Int) return NumberFormat.integer(v, defaultCulture);
-  public static var defaultFloatFormatter : Float -> String = function(v : Float) return NumberFormat.number(v, 2, defaultCulture);
+  public static var defaultFloatFormatter : Float -> String = function(v : Float) return NumberFormat.format(v, "#,#.#####", defaultCulture);
   public static var defaultStringFormatter : String -> String = function(v : String) return v;
   public static var defaultBoolFormatter : Bool -> String = function(v : Bool) return v ? "✓" : "✕";
   public static var defaultDateTimeFormatter : DateTime -> String = function(v : DateTime) return DateFormat.dateShort(v, defaultCulture);
@@ -122,10 +163,28 @@ class DefaultStyle implements IStyle {
     return new StringBlock([s]);
   };
 
+  public static var defaultAligner : Aligner = function(value : CellValue, type : CellType)
+    return switch [value, type] {
+      case [_, HeaderCompact], [BoolCell(_), _]:
+        Center;
+      case [_, Header], [StringCell(_), _], [Empty, _]:
+        Left;
+      case [BoolCell(_), _], [DateTimeCell(_), _],
+           [TimeCell(_), _], [NA, _]:
+        Right;
+      case [IntCell(_), _], [FloatCell(_), _]:
+        OnSymbol(defaultCulture.number.separatorDecimalNumber);
+      case [_, _]:
+        throw 'unmatched pattern $value, $type';
+    };
+
   public var type(get, set) : CellType;
   public var maxHeight(get, set) : Null<Int>;
+  public var minHeight(get, set) : Int;
   public var maxWidth(get, set) : Null<Int>;
+  public var minWidth(get, set) : Int;
   public var formatter(get, set) : Formatter;
+  public var aligner(get, set) : Aligner;
 
   public function new() {}
 
@@ -141,24 +200,45 @@ class DefaultStyle implements IStyle {
   function set_maxHeight(value : Null<Int>) : Null<Int>
     return defaultMaxHeight = value;
 
+  function get_minHeight() : Int
+    return defaultMinHeight;
+
+  function set_minHeight(value : Int) : Int
+    return defaultMinHeight = value;
+
   function get_maxWidth() : Null<Int>
     return defaultMaxWidth;
 
   function set_maxWidth(value : Null<Int>) : Null<Int>
     return defaultMaxWidth = value;
 
+  function get_minWidth() : Int
+    return defaultMinWidth;
+
+  function set_minWidth(value : Int) : Int
+    return defaultMinWidth = value;
+
   function get_formatter() : Formatter
     return defaultFormatter;
 
   function set_formatter(value : Formatter) : Formatter
     return defaultFormatter = value;
+
+  function get_aligner() : Aligner
+    return defaultAligner;
+
+  function set_aligner(value : Aligner) : Aligner
+    return defaultAligner = value;
 }
 
 interface IStyle {
   public var type(get, set) : CellType;
+  public var minWidth(get, set) : Int;
   public var maxWidth(get, set) : Null<Int>;
+  public var minHeight(get, set) : Int;
   public var maxHeight(get, set) : Null<Int>;
   public var formatter(get, set) : Formatter;
+  public var aligner(get, set) : Aligner;
 }
 
 enum CellType {
@@ -180,3 +260,4 @@ enum Border {
 }
 
 typedef Formatter = CellValue -> Null<Int> -> StringBlock;
+typedef Aligner = CellValue -> CellType -> CellAlign;
