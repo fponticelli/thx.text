c35b46c0b29092233d7c500bb803a94c8913117d
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-12-23 10:22:41 -0700

    fix doc

--- submit.sh
+++ submit.sh
@@ -4,5 +4,5 @@ cd doc
 haxe build_doc.hxml || { cd ..; echo "build_doc failed"; exit 1; }
 cd ..
 rm -f thx.text.zip
-zip -r thx.text.zip src doc/ImportCore.hx test extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
+zip -r thx.text.zip src doc/ImportText.hx test extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.text.zip
